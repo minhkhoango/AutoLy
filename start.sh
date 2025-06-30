f32af030a4c2e425be32c4b038f00c639b44a09e
Khoa Ngo <ngominhkhoa2006@gmail.com>
2025-06-30 21:21:09 +0700

    fix ./start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# This is the fix.
-# We are telling the Python interpreter to add the './app' directory
-# to the list of places it looks for modules.
-export PYTHONPATH="${PYTHONPATH}:."
+# 1. Walk into the room where the code lives.
+cd app
 
-# Now when Gunicorn runs your app and your app tries to import 'validation',
-# Python will know to look inside the 'app' directory and will find it.
-gunicorn --worker-class uvicorn.workers.UvicornWorker app.myapp:app
+# 2. Now, run the app. Python will find all its sibling files.
+gunicorn --worker-class uvicorn.workers.UvicornWorker myapp:app
